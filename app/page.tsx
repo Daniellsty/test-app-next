e65afa1895d8e1c18c5a9fc06a2c5cb2d047c5ee
danyallsty <danyallsty04@gmail.com>
2025-11-24 21:45:55 +0330

    completed home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import { LoginForm } from "@/components/shared";
+import ThemeToggle from "@/components/ui/ThemeToggle/ThemeToggle";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex  items-center justify-center  w-full h-screen">
+      <div className="absolute top-10 left-10">
+        <ThemeToggle />
+      </div>
+      <div className="min-h-60  min-w-[20rem] ">
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
